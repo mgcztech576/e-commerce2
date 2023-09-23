477ccefbd6ab56a67e1499f9e415fe413e3ea45a
mgcztech576 <127584002+mgcztech576@users.noreply.github.com>
2023-09-23 23:14:35 +0900

    aaa

--- src/main/java/marketing/ecommerce/client/basis/Customer.java
+++ src/main/java/marketing/ecommerce/client/basis/Customer.java
@@ -1,14 +1,26 @@
-package marketing.ecommerce.client.basis;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.envers.AuditOverride;
-
-@Entity @Getter @Builder @NoArgsConstructor @AllArgsConstructor
-public class Customer {@Id @Column(name="id",nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name; private String password;
-}
+//package marketing.ecommerce.client.basis;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import marketing.ecommerce.client.SignUp;
+//import org.hibernate.envers.AuditOverride;
+//import java.time.LocalDate;
+//@Entity @Getter @Builder @NoArgsConstructor
+//@AllArgsConstructor
+//@AuditOverride(forClass = Basis.class)
+//public class Customer extends Basis
+//{@Id @Column(name="id",nullable = false)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//    private String password;
+//    private LocalDate birth;
+//    public static Customer from(SignUp form){
+//        return Customer.builder()
+//                .name(form.getName())
+//                .password(form.getPassword())
+//                .birth(form.getBirth()).build();
+//    }
+//}
---
+++ src/main/java/marketing/ecommerce/servicetest/SingupCustomerService.java
@@ -0,0 +1,11 @@
+//package marketing.ecommerce.servicetest;
+//
+//import marketing.ecommerce.client.SignUp;
+//import marketing.ecommerce.client.basis.Customer;
+//import marketing.ecommerce.client.repository.CustomerRepository;
+//public class SingupCustomerService {
+//    private final CustomerRepository customerRepository;
+//    public Customer signUp(SignUp form){
+//        return customerRepository.save(Customer.from(form))
+//    }
+//}
